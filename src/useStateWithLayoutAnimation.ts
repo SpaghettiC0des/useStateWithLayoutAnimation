7c8fb2a2268236a68eaf739f452fa9db8217d101
Karl Marx Lopez <karlmarx.webdev@gmail.com>
2020-09-10 02:46:59 +0800

    feat: initial work on `useStateWithLayoutAnimation`

---
+++ src/useStateWithLayoutAnimation.ts
@@ -0,0 +1,44 @@
+import {useState, useRef, useEffect, SetStateAction, useCallback} from 'react';
+import {LayoutAnimation, NativeModules} from 'react-native';
+import {StateSetter, LayoutAnimationFn, OnAnimationDidEndFn} from './types';
+
+const {UIManager} = NativeModules;
+
+const useStateWithLayoutAnimation = <S>(
+  initialValue: S,
+  setLayoutAnimationEnabledExperimental = true,
+): [S, StateSetter<S>] => {
+  useEffect(() => {
+    if (setLayoutAnimationEnabledExperimental) {
+      UIManager.setLayoutAnimationEnabledExperimental &&
+        UIManager.setLayoutAnimationEnabledExperimental(true);
+    }
+  }, [setLayoutAnimationEnabledExperimental]);
+
+  const [state, setState] = useState<S>(initialValue);
+
+  const curriedSetState = useCallback(
+    (animationFn: LayoutAnimationFn) => (
+      nextState: SetStateAction<S>,
+      onAnimationDidEnd?: OnAnimationDidEndFn,
+    ) => {
+      animationFn(onAnimationDidEnd);
+      setState(nextState);
+    },
+    [],
+  );
+
+  const spring = curriedSetState(LayoutAnimation.spring);
+  const linear = curriedSetState(LayoutAnimation.linear);
+  const easeInEaseOut = curriedSetState(LayoutAnimation.easeInEaseOut);
+
+  const stateSetters = useRef({
+    spring,
+    linear,
+    easeInEaseOut,
+  }).current;
+
+  return [state, stateSetters];
+};
+
+export default useStateWithLayoutAnimation;
